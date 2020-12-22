cad7b588c4c47a968cb198ea69e7e7793fba3ecd
admin <admin@DESKTOP-2NPQ13G>
2020-12-22 14:53:50 +0530

    first commit...

---
+++ src/main/java/com/capgemini/fitnesscasestudy/FitnessCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.fitnesscasestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessCaseStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/fitnesscasestudy/FitnessCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.fitnesscasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
